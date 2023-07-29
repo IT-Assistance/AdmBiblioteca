eb2f1f19c000fcc5f341c6d20397d0b62f340f58
Aneudys Ortiz <mabelortiz0711@gmail.com>
2023-07-29 19:29:13 -0400

    Initial Commit

---
+++ CapaNegocios/Acciones/AccionesBase.cs
@@ -0,0 +1,14 @@
+﻿using CapaDatos.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios.Acciones
+{
+    public class AccionesBase
+    {
+        public DbLibraryEntityDataContext dbLibContext = new DbLibraryEntityDataContext();
+    }
+}
---
+++ CapaNegocios/Acciones/AccionesConsulta.cs
@@ -0,0 +1,30 @@
+﻿using CapaDatos.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios.Acciones
+{
+    public class AccionesConsulta:AccionesBase
+    {
+        #region Listados
+        public List<TM_Libro> listLibros()
+        {
+            return dbLibContext.TM_Libros.ToList();
+        }
+
+        public List<TM_Salones_Reunione> listSalones()
+        {
+            return dbLibContext.TM_Salones_Reuniones.ToList();
+        }
+
+        public List<TM_Espacio_Trabajo> listEspacioTrabajo()
+        {
+            return dbLibContext.TM_Espacio_Trabajos.ToList();
+        }
+
+        #endregion
+    }
+}
